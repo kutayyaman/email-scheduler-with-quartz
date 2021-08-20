6cc9c6584981110fea81c661f907ab1c770ecab7
Kutay Yaman <yamankutay1@gmail.com>
2021-08-21 00:29:31 +0300

    project initialized

---
+++ src/test/java/com/example/emailschedulerwithquartz/EmailSchedulerWithQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailschedulerwithquartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSchedulerWithQuartzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
